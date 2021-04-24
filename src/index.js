ec2d0631d21adc30d1e4f506c8d7ccc7901c542b
ᑭᖇᗴᔕᕼ ᗝᑎƳᗴᗴ <presh_onyee@outlook.com>
2021-04-24 14:24:37 -0700

    feat: initial prototype working

---
+++ src/index.js
@@ -0,0 +1,67 @@
+require("colors");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const templates = require("./templates");
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "Select the language your component is written in:",
+      name: "language",
+      choices: ["javascript", "typescript"],
+    },
+    {
+      type: "input",
+      message: "What is the name of your component?",
+      name: "compName",
+    },
+  ])
+  .then((answers) => {
+    if (answers.language === "typescript") {
+      const componentName = answers.compName;
+
+      if (!componentName || componentName === "") {
+        console.error("Please supply a valid component name".red);
+        process.exit(1);
+      }
+
+      console.log(
+        "Creating Component Templates with name: ".blue + componentName.magenta
+      );
+
+      const componentDirectory = `${__dirname}/${componentName}`;
+
+      if (fs.existsSync(componentDirectory)) {
+        console.error(`Component ${componentName} already exists.`.red);
+        process.exit(1);
+      }
+
+      fs.mkdirSync(componentDirectory);
+
+      const generatedTemplates = templates.map((template) =>
+        template(componentName)
+      );
+
+      generatedTemplates.forEach((template) => {
+        fs.writeFileSync(
+          `${componentDirectory}/${componentName}${template.extension}`,
+          template.content
+        );
+      });
+
+      console.log(
+        "Successfully created component under: ".yellow +
+          componentDirectory.magenta
+      );
+    }
+    console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+      console.log(error);
+    }
+  });
